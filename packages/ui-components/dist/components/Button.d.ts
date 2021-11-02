8f675d2dca4e3ed2cd6b2300c5536064279ee09e
ERRE216 <racklif.abel.gm@gmail.com>
2021-11-01 21:20:20 -0400

    added dist to .gitignore

--- packages/ui-components/dist/components/Button.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export interface ButtonProps extends StyledButtonProps {
-    text: string;
-}
-interface StyledButtonProps {
-    backgroundColor: string;
-}
-export declare function Button({ text, backgroundColor }: ButtonProps): JSX.Element;
-export {};
